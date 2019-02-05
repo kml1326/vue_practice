904aef209ca3ef53a918386b9c8f44e6232b124d
kml1326 <kmlraj131996@gmail.com>
2019-02-05 08:55:18 +0530

    some changes in signup page of realworld application

--- real-world-ex/src/views/SignUp.vue
+++ real-world-ex/src/views/SignUp.vue
@@ -3,10 +3,33 @@
     <p>Sign Up</p>
     <router-link :to="{ name: 'sign-in' }" class="nav-link">Have an account?</router-link>
     <form>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+      <input type="text" placeholder="Username" name="username" v-model="message" value="{message}" v-on:change="handleChange($event)" />
+      <p>{{message}}</p>
+      <input type="text" placeholder="Name" v-model="name" value="name" />
+      <input type="email" placeholder="Email" name="email" v-on:change="handleChange" />
+      <input type="password" placeholder="password" name="password" v-on:change="handleChange" />
       <input type="button" value="Sign up">
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      name: '',
+      email: '',
+      password: '',
+      message: ''
+    }
+  },
+  methods: {
+    handleChange: function(e) {
+      console.log(e, e.target.name, e.target.value)
+      
+    }
+  }
+}
+</script>
+
